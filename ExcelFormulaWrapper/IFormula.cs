f436b7a80363d9301fc2d28c5c4899cd49655fdf
Galina Litvinova <Galina.Litvinova@jti.com>
2023-11-17 23:18:38 +0300

    Add project files.

---
+++ ExcelFormulaWrapper/IFormula.cs
@@ -0,0 +1,154 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ExcelFormulaWrapper
+{
+    public interface IFormula
+    {
+        public string Build();
+    }
+
+    public abstract class AbstractFormula : IFormula
+    {
+        public abstract string Build();
+        public override string ToString()
+        {
+            return Build();
+        }
+    }
+
+    public class Const : AbstractFormula
+    {
+        private readonly string _c;
+
+        public Const(string c)
+        {
+            _c = c;
+        }
+
+        public Const(ValueType t)
+        {
+            _c = t.ToString() ?? "";
+        }
+        public override string Build()
+        {
+            return _c;
+        }
+    }
+
+    public class Column : AbstractFormula
+    {
+        private int _index;
+
+        public Column(int index) : base()
+        {
+            _index = index;
+        }
+
+        public override string Build()
+        {
+            return "{{" + _index + "}}";
+        }
+    }
+
+    public abstract class Operator : AbstractFormula
+    {
+        private IFormula _left, _right;
+        private string _operator;
+
+        public Operator(IFormula left, string @operator, IFormula right)
+        {
+            _left = left;
+            _right = right;
+            _operator = @operator;
+        }
+
+        public override string Build()
+        {
+            return _left.Build() + _operator + _right.Build();
+        }
+    }
+
+    public abstract class TwoOperands : AbstractFormula
+    {
+        private IFormula _left, _right;
+        private string _operator;
+
+        public TwoOperands(string @operator, IFormula left, IFormula right)
+        {
+            _left = left;
+            _right = right;
+            _operator = @operator;
+        }
+
+        public override string Build()
+        {
+            return _operator + "(" + _left.Build() + "," + _right.Build() + ")";
+        }
+    }
+
+    public abstract class ThreeOperands : AbstractFormula
+    {
+        private IFormula _first, _second, _third;
+        private string _operator;
+
+        public ThreeOperands(string @operator, IFormula first, IFormula second, IFormula third)
+        {
+            _operator = @operator;
+            _first = first;
+            _second = second;
+            _third = third;
+        }
+
+        public override string Build()
+        {
+            return _operator + "(" + _first.Build() + ", " + _second.Build() + ", " + _third.Build() + ")";
+        }
+    }
+
+    public class If : ThreeOperands
+    {
+        public If(IFormula @if, IFormula @then, IFormula @else) : base("IF", @if,@then, @else)
+        {
+        }
+    }
+
+    public class Greater : Operator
+    {
+        public Greater(IFormula left, IFormula right) : base(left, ">", right)
+        {
+        }
+    }
+
+    public class Subtract : Operator
+    {
+        public Subtract(IFormula left, IFormula right) : base(left, "-", right)
+        {
+        }
+    }
+    public class Add : Operator
+    {
+        public Add(IFormula left, IFormula right) : base(left, "+", right)
+        {
+        }
+    }
+
+    public class Equals : Operator
+    {
+        public Equals(IFormula left, IFormula right) : base(left, "=", right)
+        {
+        }
+    }
+
+    public class Min: TwoOperands
+    {
+        public Min(IFormula left, IFormula right) : base("MIN", left, right)
+        {
+        }
+    }
+    public class Max : TwoOperands
+    {
+        public Max(IFormula left, IFormula right) : base("MAX", left, right)
+        {
+        }
+    }
+}
---
+++ ExcelFormulaWrapper/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+using ExcelFormulaWrapper;
+
+Console.WriteLine("Hello, World!");
+var col1 = 3;
+var col2 = 8;
+var column1 = new Column(col1);
+var  column2 = new Column(col2);
+/* "IF({{" + column.Index + "}}>{{" + planned.Index + "}},{{" + column.Index + "}}-{{" +
+planned.Index + "}},0)" */
+var formula = new If(
+    @if: new Greater(column1, column2),
+    then:new Subtract(column1, column2),
+    @else:new Const("0"));
+
+var f= formula.Build();
+Console.WriteLine(f);
+
+/*
+ $"IF({{{{{factoryNameColumn.Index}}}}}=\"PETRO\"," +
+ $"MAX({{{{{plannedColumn.Index}}}}} - {{{{{stockColumn.Index}}}}} - {{{{{productionMonthColumn.Index}}}}}, 0)," +
+$"MAX({rhwsSum} + {{{{{plannedColumn.Index}}}}}-{{{{{stockColumn.Index}}}}},0)";
+*/
+var factoryNameColumn = new Column(1);
+var plannedColumn = new Column(2);
+var stockColumn = new Column(3);
+var productionMonthColumn = new Column(4);
+var rwhs = new Const(333);
+var formula2 = new If(
+    new Equals(factoryNameColumn, new Const("PETRO")),
+    new Max(new Subtract(new Subtract(plannedColumn, stockColumn), productionMonthColumn), new Const("0")),
+    new Max(new Subtract(new Add(rwhs, plannedColumn), stockColumn), new Const(0))
+    );
+
+Console.WriteLine(formula2);
+var petro = new Const("Petro");
+/*
+ $"IF({{{{{factoryNameColumn.Index}}}}}=\"PETRO\"," +
+ $"MAX({{{{{plannedColumn.Index}}}}} - {{{{{stockColumn.Index}}}}} - {{{{{productionMonthColumn.Index}}}}}, 0)," +
+$"MAX({rhwsSum} + {{{{{plannedColumn.Index}}}}}-{{{{{stockColumn.Index}}}}},0)";
+*/
+var formula3 = $"IF({factoryNameColumn} = {petro}, MAX({plannedColumn} + {stockColumn} - {productionMonthColumn}, 0), MAX({rwhs} + {plannedColumn} - {stockColumn}, 0))";
+Console.WriteLine(formula3);
